bc4ae09a1f47447f6b513eec4c8f3156917ec4a2
Patrick Meehan <patrickmeehan.git@fallguyconsulting.com>
2021-10-01 20:12:07 -0700

    pass tag on command line

--- build.sh
+++ build.sh
@@ -8,6 +8,11 @@ pushd ${SCRIPT_HOME}
     
     TAG=${1:-$TAG}
 
+    if [[ -z "$TAG" ]]; then
+        echo "TAG is undefined; please specify a tag on the command line or in .env."
+        exit 1
+    fi
+
     source $VOL_NODE_CPP/ops/make-version-header.sh
     docker build -f ./volition-base/Dockerfile -t volition-build-base .
     docker build -f ./Dockerfile --no-cache -t $TAG $VOL_NODE_CPP
